979df838ebd806ec9aff389187639bcdfddccc68
aelwardi <a.elwardi98@gmail.com>
2025-06-24 13:16:15 +0200

    page section design with tailwind

---
+++ src/Controller/SectionController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class SectionController extends AbstractController
+{
+    #[Route('/section', name: 'app_section')]
+    public function index(): Response
+    {
+        return $this->render('section/index.html.twig');
+    }
+}
