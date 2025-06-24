b4376c705a25dbd1095a4fe61ff962df10cd7ff8
aelwardi <a.elwardi98@gmail.com>
2025-06-24 17:45:50 +0200

    fixtures howWorks and step added

---
+++ src/Controller/HowWorksController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\HowWorksRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HowWorksController extends AbstractController
+{
+    #[Route('/howWork', name: 'app_how_works')]
+    public function index(HowWorksRepository $howWorksRepository): Response
+    {
+        $howWorks = $howWorksRepository->findAll();
+        dd($howWorks);
+        return $this->render('how_works/index.html.twig');
+    }
+}
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,8 @@ namespace App\DataFixtures;
 
 use App\Entity\Laundry;
 use App\Entity\Section;
+use App\Entity\HowWorks;
+use App\Entity\Step;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -54,6 +56,114 @@ class AppFixtures extends Fixture
         $section3->setDescription('Deyan Dimitrov, PDG de Laundryheap');
         $section3->setLaundry($laundry);
         $manager->persist($section3);
+
+        $howWorks = new HowWorks();
+        $howWorks->setIndice('Souple');
+        $howWorks->setTitle('Planifiez votre collecte');
+        $howWorks->setImage('img10.png');
+        $howWorks->setRang(1);
+        $howWorks->setDescription('Planifiez votre journée en toute simplicité. Choisissez un délai de collecte et de livraison qui vous convient.');
+        $howWorks->setLaundry($laundry);
+        $manager->persist($howWorks);
+
+        $step = new Step();
+        $step->setTitle('Réservez en ligne.');
+        $step->setIcon('svg1.svg');
+        $step->setRang(1);
+        $step->setHowWork($howWorks);
+        $howWorks->addStep($step);
+        $manager->persist($step);
+
+        $step1 = new Step();
+        $step1->setTitle('Créneaux disponibles le week-end et le soir.');
+        $step1->setIcon('svg2.svg');
+        $step1->setRang(2);
+        $step1->setHowWork($howWorks);
+        $howWorks->addStep($step1);
+        $manager->persist($step1);
+
+        $howWorks1 = new HowWorks();
+        $howWorks1->setIndice('Souple');
+        $howWorks1->setTitle('Planifiez votre collecte');
+        $howWorks1->setImage('img10.png');
+        $howWorks1->setRang(2);
+        $howWorks1->setDescription('Planifiez votre journée en toute simplicité. Choisissez un délai de collecte et de livraison qui vous convient.');
+        $howWorks1->setLaundry($laundry);
+        $manager->persist($howWorks1);
+
+        $step3 = new Step();
+        $step3->setTitle('Emballez un sac par service.');
+        $step3->setIcon('svg3.svg');
+        $step3->setRang(1);
+        $step3->setHowWork($howWorks1);
+        $howWorks1->addStep($step3);
+        $manager->persist($step3);
+
+        $step4 = new Step();
+        $step4->setTitle('Pas besoin de compter ou de peser vos articles');
+        $step4->setIcon('svg4.svg');
+        $step4->setRang(2);
+        $step4->setHowWork($howWorks1);
+        $howWorks1->addStep($step4);
+        $manager->persist($step4);
+
+        $howWorks2 = new HowWorks();
+        $howWorks2->setIndice('Souple');
+        $howWorks2->setTitle('Planifiez votre collecte');
+        $howWorks2->setImage('img10.png');
+        $howWorks2->setRang(3);
+        $howWorks2->setDescription('Planifiez votre journée en toute simplicité. Choisissez un délai de collecte et de livraison qui vous convient.');
+        $howWorks2->setLaundry($laundry);
+        $manager->persist($howWorks2);
+
+        $step5 = new Step();
+        $step5->setTitle('Mises à jour régulières des commandes');
+        $step5->setIcon('svg5.svg');
+        $step5->setRang(1);
+        $step5->setHowWork($howWorks2);
+        $howWorks2->addStep($step5);
+        $manager->persist($step5);
+
+        $step6 = new Step();
+        $step6->setTitle('Suivi des conducteurs en temps réel');
+        $step6->setIcon('svg6.svg');
+        $step6->setRang(2);
+        $step6->setHowWork($howWorks2);
+        $howWorks2->addStep($step6);
+        $manager->persist($step6);
+
+        $howWorks3 = new HowWorks();
+        $howWorks3->setIndice('Souple');
+        $howWorks3->setTitle('Planifiez votre collecte');
+        $howWorks3->setImage('img10.png');
+        $howWorks3->setRang(4);
+        $howWorks3->setDescription('Planifiez votre journée en toute simplicité. Choisissez un délai de collecte et de livraison qui vous convient.');
+        $howWorks3->setLaundry($laundry);
+        $manager->persist($howWorks3);
+
+        $step7 = new Step();
+        $step7->setTitle('Délai d\'exécution de 24 heures.');
+        $step7->setIcon('svg7.svg');
+        $step7->setRang(1);
+        $step7->setHowWork($howWorks3);
+        $howWorks3->addStep($step7);
+        $manager->persist($step7);
+        
+        $step8 = new Step();
+        $step8->setTitle('Suivi des commandes en temps réel');
+        $step8->setIcon('svg5.svg');
+        $step8->setRang(2);
+        $step8->setHowWork($howWorks3);
+        $howWorks3->addStep($step8);
+        $manager->persist($step8);
+
+        $step9 = new Step();
+        $step9->setTitle('Facile à reprogrammer');
+        $step9->setIcon('svg8.svg');
+        $step9->setRang(3);
+        $step9->setHowWork($howWorks3);
+        $howWorks3->addStep($step9);
+        $manager->persist($step9);
 
         $manager->flush();
     }
