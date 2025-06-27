0bc795ab40d6f9aa0c302f8a2ce831daaa3ec99f
aelwardi <a.elwardi98@gmail.com>
2025-06-27 13:56:52 +0200

    login & signup in the same page - done

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -5,27 +5,51 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Attribute\Route;
-use App\Form\RegisterUserForm;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Doctrine\ORM\EntityManagerInterface;
+use App\Form\RegisterUserForm;
 use App\Entity\User;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Attribute\Route;
 
 final class AuthController extends AbstractController
 {
     #[Route('/auth', name: 'app_auth')]
-    public function index(Request $request, EntityManagerInterface $entityManager): Response
+    public function index(Request $request,
+        EntityManagerInterface $entityManager,
+        UserPasswordHasherInterface $passwordHasher,
+        AuthenticationUtils $authenticationUtils): Response
     {
         $user = new User();
-        $signup = $this->createForm(RegisterUserForm::class);
+        $signup = $this->createForm(RegisterUserForm::class, $user);
         $signup->handleRequest($request);
         if($signup->isSubmitted() && $signup->isValid()) {
             $user = $signup->getData();
             $user->setRoles(['ROLE_USER']);
+            $user->setPassword(
+                $passwordHasher->hashPassword(
+                    $user,
+                    $signup->get('password')->getData()
+                )
+            );
             $entityManager->persist($user);
             $entityManager->flush();
+            return $this->redirectToRoute('app_auth');
         }
+
+        $lastUsername = $authenticationUtils->getLastUsername();
+        $loginError = $authenticationUtils->getLastAuthenticationError();
+
         return $this->render('auth/index.html.twig', [
             'signup' => $signup->createView(),
+            'last_username' => $lastUsername,
+            'login_error' => $loginError,
         ]);
+    }
+
+    #[Route('/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        return $this->redirectToRoute('app_auth');
     }
 }
