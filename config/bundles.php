f3dc5a1fdcf849a09338a6d2343cc458897f88a6
aelwardi <a.elwardi98@gmail.com>
2025-07-04 02:40:49 +0200

    changement de structure webpack par assetMapper - rend auth responsive

--- config/bundles.php
+++ config/bundles.php
@@ -8,7 +8,6 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
-    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './templates/**/*.twig',
+    './assets/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
